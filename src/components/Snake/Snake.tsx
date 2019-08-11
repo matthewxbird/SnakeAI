8d31385a2db161d2ad06740f9370b50815bc5f13
Matthew Bird <mbird1989@hotmail.com>
2019-08-11 20:58:00 +0100

    move snake into its own class

---
+++ src/components/Snake/Snake.tsx
@@ -0,0 +1,11 @@
+class Snake {
+  public width: number = 10;
+  public speed: number = 2;
+
+  public xPos: number = 50;
+  public yPos: number = 50;
+
+  constructor() {}
+}
+
+export default Snake;
--- src/components/SnakeGame/SnakeGame.tsx
+++ src/components/SnakeGame/SnakeGame.tsx
@@ -1,18 +1,13 @@
+import Snake from "../Snake/Snake";
+
 class SnakeGame {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
 
-  private snakeWidth: number = 10;
-  private snakeSpeed: number = 2;
+  private _snake: Snake;
 
   private height: number = 400;
   private width: number = 400;
-
-  private xLimit: number = this.width - this.snakeWidth;
-  private yLimit: number = this.height - this.snakeWidth;
-
-  private xPos: number = 50;
-  private yPos: number = 50;
 
   private xDir: number = 1;
   private yDir: number = 0;
@@ -22,23 +17,33 @@ class SnakeGame {
     this.canvas.width = this.width;
     this.canvas.height = this.height;
     this.ctx = this.canvas.getContext("2d");
+    this._snake = new Snake();
   }
 
   public changeDirection(key: string) {
+    console.log(key);
     switch (key) {
       case "ArrowUp":
+      case "W":
+      case "w":
         this.xDir = 0;
         this.yDir = -1;
         break;
       case "ArrowDown":
+      case "S":
+      case "s":
         this.xDir = 0;
         this.yDir = 1;
         break;
       case "ArrowLeft":
+      case "A":
+      case "a":
         this.xDir = -1;
         this.yDir = 0;
         break;
       case "ArrowRight":
+      case "D":
+      case "d":
         this.xDir = 1;
         this.yDir = 0;
         break;
@@ -46,27 +51,31 @@ class SnakeGame {
   }
 
   public update(): void {
-    this.xPos += this.xDir * this.snakeSpeed;
-    this.yPos += this.yDir * this.snakeSpeed;
+    this._snake.xPos += this.xDir * this._snake.speed;
+    this._snake.yPos += this.yDir * this._snake.speed;
 
     this.ensureBounds();
   }
 
   public ensureBounds(): void {
-    if (this.xPos < 0) {
-      this.xPos = 0;
+    if (this._snake.xPos < 0) {
+      this._snake.xPos = 0;
     }
 
-    if (this.yPos < 0) {
-      this.yPos = 0;
+    if (this._snake.yPos < 0) {
+      this._snake.yPos = 0;
     }
 
-    if (this.xPos > this.xLimit) {
-      this.xPos = this.xLimit;
+    let xLimit: number = this.width - this._snake.width;
+
+    if (this._snake.xPos > xLimit) {
+      this._snake.xPos = xLimit;
     }
 
-    if (this.yPos > this.yLimit) {
-      this.yPos = this.yLimit;
+    let yLimit: number = this.height - this._snake.width;
+
+    if (this._snake.yPos > yLimit) {
+      this._snake.yPos = yLimit;
     }
   }
 
@@ -81,7 +90,12 @@ class SnakeGame {
 
   public drawSnake() {
     this.ctx.fillStyle = "#f00";
-    this.ctx.fillRect(this.xPos, this.yPos, this.snakeWidth, this.snakeWidth);
+    this.ctx.fillRect(
+      this._snake.xPos,
+      this._snake.yPos,
+      this._snake.width,
+      this._snake.width
+    );
   }
 }
 
