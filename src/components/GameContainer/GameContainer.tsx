bc8d31192393f3130f66817bcee3532245dbc4e2
Matthew Bird <mbird1989@hotmail.com>
2019-08-11 22:04:13 +0100

    added missing _'s to private members

--- src/components/GameContainer/GameContainer.tsx
+++ src/components/GameContainer/GameContainer.tsx
@@ -2,23 +2,23 @@ import Snake from "../Snake/Snake";
 import Position from "../Position";
 
 class GameContainer {
-  private ctx: CanvasRenderingContext2D;
+  private _ctx: CanvasRenderingContext2D;
 
   private _snake: Snake;
 
-  private height: number;
-  private width: number;
+  private _height: number;
+  private _width: number;
 
-  private xDir: number = 1;
-  private yDir: number = 0;
+  private _xDir: number = 1;
+  private _yDir: number = 0;
 
   constructor() {
     const canvas: HTMLCanvasElement = document.getElementById(
       "rootCanvas"
     ) as HTMLCanvasElement;
-    this.width = canvas.width;
-    this.height = canvas.height;
-    this.ctx = canvas.getContext("2d");
+    this._width = canvas.width;
+    this._height = canvas.height;
+    this._ctx = canvas.getContext("2d");
     this._snake = new Snake(new Position(50, 50), 10, 2, "#f00");
   }
 
@@ -28,34 +28,34 @@ class GameContainer {
       case "ArrowUp":
       case "W":
       case "w":
-        this.xDir = 0;
-        this.yDir = -1;
+        this._xDir = 0;
+        this._yDir = -1;
         break;
       case "ArrowDown":
       case "S":
       case "s":
-        this.xDir = 0;
-        this.yDir = 1;
+        this._xDir = 0;
+        this._yDir = 1;
         break;
       case "ArrowLeft":
       case "A":
       case "a":
-        this.xDir = -1;
-        this.yDir = 0;
+        this._xDir = -1;
+        this._yDir = 0;
         break;
       case "ArrowRight":
       case "D":
       case "d":
-        this.xDir = 1;
-        this.yDir = 0;
+        this._xDir = 1;
+        this._yDir = 0;
         break;
     }
   }
 
   public update(): void {
     const existingPos = this._snake.getPos();
-    const newX = existingPos.GetX() + this.xDir * this._snake.getSpeed();
-    const newY = existingPos.GetY() + this.yDir * this._snake.getSpeed();
+    const newX = existingPos.GetX() + this._xDir * this._snake.getSpeed();
+    const newY = existingPos.GetY() + this._yDir * this._snake.getSpeed();
     this._snake.setPos(newX, newY);
     this.ensureBounds();
   }
@@ -69,13 +69,13 @@ class GameContainer {
       this._snake.setPos(this._snake.getPos().GetX(), 0);
     }
 
-    let xLimit: number = this.width - this._snake.getWidth();
+    let xLimit: number = this._width - this._snake.getWidth();
 
     if (this._snake.getPos().GetX() > xLimit) {
       this._snake.setPos(xLimit, this._snake.getPos().GetY());
     }
 
-    let yLimit: number = this.height - this._snake.getWidth();
+    let yLimit: number = this._height - this._snake.getWidth();
 
     if (this._snake.getPos().GetY() > yLimit) {
       this._snake.setPos(this._snake.getPos().GetX(), yLimit);
@@ -83,7 +83,7 @@ class GameContainer {
   }
 
   public clearScreen(): void {
-    this.ctx.clearRect(0, 0, this.width, this.height);
+    this._ctx.clearRect(0, 0, this._width, this._height);
   }
 
   public draw(): void {
@@ -92,7 +92,7 @@ class GameContainer {
   }
 
   public drawSnake() {
-    this._snake.draw(this.ctx);
+    this._snake.draw(this._ctx);
   }
 }
 
